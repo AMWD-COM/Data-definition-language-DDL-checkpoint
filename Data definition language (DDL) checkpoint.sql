a820f4ec824bdad66184f8f4980cc9b9cc4b40ba
AHMED MIZI <85645982+AMWD-COM@users.noreply.github.com>
2022-03-16 12:52:30 +0100

    Update Data definition language (DDL) checkpoint.sql
    remove comments

--- Data definition language (DDL) checkpoint.sql
+++ Data definition language (DDL) checkpoint.sql
@@ -3,7 +3,7 @@
 
 /**  -CREATE TABLE-
 
-//'PRAGMA' specific commands to activate foreign keys
+//'PRAGMA' specific commands to activate foreign keys*/
 
 PRAGMA foreign_keys = ON; 
 
@@ -27,27 +27,20 @@ total_amount NUMBER,
 FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
 FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
-*/
 
-/** Joint TABLES 
-
-SELECT *
-    FROM orders
-    LEFT JOIN customers ON (orders.customer_id = customers.customer_id)
-    LEFT JOIN products ON (orders.product_id = products.product_id)
-    
-*/
 
 /** ALTER TABLES */
 
-/** ADD category column to products
+/** ADD category column to products*/
 
 ALTER TABLE products ADD COLUMN category VARCHAR(20);
 
-*/
+
 
 /** ADD order_date column to orders*/
 
 ALTER TABLE orders ADD COLUMN order_date DATETIME;
+
+
 
 
